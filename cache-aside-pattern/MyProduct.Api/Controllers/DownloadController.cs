4c7dca4eed5930a9d42c7dd0f99b0f6f13028695
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-06-20 22:08:27 +0200

    Adding Cache-Aside Pattern

---
+++ cache-aside-pattern/MyProduct.Api/Controllers/DownloadController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyProduct.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMemoryCache _cache;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+         var downloadedByteArray = await GetOrInitializeAsync(DownloadFromGithub, "downloadedByteArray");
+         return File(downloadedByteArray, "text/json");
+        }
+
+        private async Task<T> GetOrInitializeAsync<T>(Func<Task<T>> initializeFunction, string cacheKey,
+            ushort timeoutInMinutes = 15)
+        {
+            if (_cache.TryGetValue(cacheKey, out T? cachedValue))
+            {
+                if (cachedValue != null)
+                {
+                    return cachedValue;
+                }
+            }
+
+            var initializedObject = await initializeFunction();
+            _cache.Set(cacheKey, initializedObject, TimeSpan.FromMinutes(timeoutInMinutes));
+            return initializedObject;
+        }
+
+        private async Task<byte[]> DownloadFromGithub()
+        {
+            var endpointUrl = "https://gist.githubusercontent.com/nikneem/66060dad016cabd426166f282a946f80/raw/581fe051d1b84c6799eecbdca5496b69c474292c/4DotNet-WebCasts-Caching.json";
+            var client = _httpClientFactory.CreateClient("Download");
+            var response = await client.GetAsync(endpointUrl);
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+
+
+        public DownloadController(IHttpClientFactory httpClientFactory, IMemoryCache cache)
+        {
+            _httpClientFactory = httpClientFactory;
+            _cache = cache;
+        }
+
+    }
+}
---
+++ cache-aside-pattern/MyProduct.Api/Program.cs
@@ -0,0 +1,49 @@
+using Polly;
+using Polly.Extensions.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddHttpClient("Download")
+    .AddPolicyHandler(GetCircuitBreakerPolicy())
+    .AddPolicyHandler(GetRetryPolicy());
+builder.Services.AddMemoryCache();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+        .WaitAndRetryAsync(4, retryAttempt => TimeSpan.FromSeconds(2));
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+        .CircuitBreakerAsync(3, TimeSpan.FromSeconds(11));
+}
